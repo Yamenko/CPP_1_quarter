2c6df064f797794bed502a1b8d2444a448b80034
Yamenko <yamenko@yandex.ru>
2022-03-06 23:53:05 +0300

    OOP_HW_6 && BJ

---
+++ BlackJack_0.0.1/BlackJack_0.0.1.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <vector>
+
+class Card
+{
+private:
+	enum Mast : char { CHERVY = 3, BUBY = 4, KRESTY = 5, PIKY = 6 };
+	enum CostCard : int { ACE = 1, TWO = 2, TREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10, JACK = 10, QUEEN = 10, KING = 10 };
+	Mast mast = BUBY;
+	CostCard cost = ACE;
+	bool crap = false;
+public:
+	Card() {};
+	~Card() { };
+	void Flip() { crap = !crap; };
+	void SetMast(Mast& mast) { this->mast = mast; };
+	void SetCostCard(CostCard& cost) { this->cost = cost; };
+
+	CostCard	GetCost() { return this->cost; };
+	Mast		GetMast() { return this->mast; };
+	bool		GetCrap() { return this->crap; };
+};
+
+class Hand : public Card
+{
+private:
+	std::vector<Card*> cardOnHand;
+public:
+	void addCard(Card* card) { cardOnHand.push_back(card); };
+	void clearHand() {	cardOnHand.clear();	};
+	int GetValue() {
+		int sumHand = 0;
+		for (auto i : cardOnHand) {
+			if (i->GetCost() == 1 && (sumHand + i->GetCost() + 10) <= 21) {
+				sumHand += i->GetCost() + 10;
+			}
+			else {
+				sumHand += i->GetCost();
+			}
+		}
+		return sumHand;
+	};
+};
+
+class GenericPlayer : public Hand {
+private:
+	std::string name = "";
+public:
+	virtual bool const IsHitting();
+	bool IsBoosted() { GetValue() > 21 ? true : false; };
+	void Bust() { std::cout << this->name << "Has a bust!" << std::endl; };
+	std::string  getName() const { return name; };
+};
+
+class Player : public GenericPlayer {
+public:
+	bool const IsHitting() override {
+		std::string answer = "";
+		answer.resize(1);
+		bool repit = true;
+		do {
+			std::cout << "Do U need a card more? (y/n)";
+			std::cin >> answer;
+			if (answer == "y") { return true; };
+			if (answer == "n") { return false; };
+		} while (true);
+	};
+	void Win() const { std::cout << getName() << " is Winer!"; };
+	void Lose() const { std::cout << getName() << " is Looser!"; };
+	void Push() const { std::cout << "Have no winer!"; };
+};
+//• virtual bool IsHitting() const - метод указывает, нужна ли дилеру еще одна карта.Если у дилера не больше 16 очков, то он берет еще одну карту.
+//• void FlipFirstCard() - метод переворачивает первую карту дилера.
+class House : public GenericPlayer {
+public:
+	bool const IsHitting() override {
+		if (GetValue() <= 16) { return true; }
+	};
+	void FlipFirstCard() {	
+		if (GetCrap()) { Flip(); };	
+	};
+};
+
+int main()
+{
+
+}
---
+++ OOP_HW_6/OOP_HW_6.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include <string>
+
+// ==========================
+//          Task1
+// ==========================
+// 1. Создать программу, которая считывает целое число типа int.
+// И поставить «защиту от дурака» : если пользователь вводит что - то кроме одного целочисленного значения, 
+// нужно вывести сообщение об ошибке и предложить ввести число еще раз.Пример неправильных введенных строк :
+//rbtrb
+//nj34njkn
+//1n
+
+// ==========================
+//          Task2
+// ==========================
+//2. Создать собственный манипулятор endll для стандартного потока вывода, который выводит два перевода строки и сбрасывает буфер.
+
+std::ostream& endll (std::ostream& out) {
+    out << std::endl << std::endl;
+    return out;
+};
+
+// ==========================
+//          Task3
+// ==========================
+//3. Реализовать класс Player, который наследует от класса GenericPlayer.У этого класса будет 4 метода:
+//• virtual bool IsHitting() const - реализация чисто виртуальной функции базового класса.
+// Метод спрашивает у пользователя, нужна ли ему еще одна карта и возвращает ответ пользователя в виде true или false.
+//• void Win() const - выводит на экран имя игрока и сообщение, что он выиграл.
+//• void Lose() const - выводит на экран имя игрока и сообщение, что он проиграл.
+//• void Push() const - выводит на экран имя игрока и сообщение, что он сыграл вничью.
+
+
+class GenericPlayer{//заглушка
+public:
+    virtual bool const IsHitting();
+    std::string  getName() const { return name; };
+private:
+    std::string name = "";
+};
+
+class Player : public GenericPlayer {
+public:
+    bool const IsHitting() override {
+        std::string answer = "";
+        answer.resize(1);
+        bool repit = true;
+        do {
+            std::cout << "Do U need a card more? (y/n)";
+            std::cin >> answer;
+            if (answer == "y") { return true; };
+            if (answer == "n") { return false; };
+            system("cls");
+        } while (true);
+    };
+    void Win() const { std::cout << getName() << " is Winer!";  };
+    void Lose() const { std::cout << getName() << " is Looser!"; };
+    void Push() const { std::cout << "Have no winer!"; };
+};
+ 
+
+// ==========================
+//          Task4
+// ==========================
+// 4. Реализовать класс House, который представляет дилера.Этот класс наследует от класса GenericPlayer.
+// У него есть 2 метода:
+//• virtual bool IsHitting() const - метод указывает, нужна ли дилеру еще одна карта.Если у дилера не больше 16 очков, то он берет еще одну карту.
+//• void FlipFirstCard() - метод переворачивает первую карту дилера.
+
+class House : public GenericPlayer {
+    bool const IsHitting() override {
+
+    }
+    void FlipFirstCard() {
+        
+    };
+};
+
+// ==========================
+//          Task5
+// ==========================
+//5. Написать перегрузку оператора вывода для класса Card.
+//Если карта перевернута рубашкой вверх(мы ее не видим), вывести ХХ, если мы ее видим, 
+//вывести масть и номинал карты.Также для класса GenericPlayer написать перегрузку оператора вывода, 
+//который должен отображать имя игрока и его карты, а также общую сумму очков его карт.
+
+int main()
+{
+    //Task 1
+    std::cout << "Task 1." << std::endl;
+    std::string myLine = "";
+    bool hasAlpha = false;
+    do {
+        hasAlpha = false;
+        std::cout << "Enter number: ";
+        std::cin >> myLine;
+
+        for (auto oneElement : myLine) {
+            if (!isdigit(oneElement)) {
+                system("cls");
+                std::cout << "U entered not a number!!!" << std::endl;
+                hasAlpha = true;
+                break;
+            }
+        }
+    } while (hasAlpha);  
+    std::cout << std::endl << "Tanks. Ur number: " << myLine << std::endl;
+
+    //Task 2
+    std::cout << "Task 2." << std::endl;
+    std::cout << "Line 1." << endll << "Line 2." << std::endl << "Line 3.";
+}
