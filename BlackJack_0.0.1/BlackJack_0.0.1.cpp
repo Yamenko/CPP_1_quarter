c6f7df41f0349899ef4beaa42704e959f245ccdf
Yamenko <yamenko@yandex.ru>
2022-04-09 00:08:44 +0300

    make homme work 1 "dificult momrnts"

--- BlackJack_0.0.1/BlackJack_0.0.1.cpp
+++ BlackJack_0.0.1/BlackJack_0.0.1.cpp
@@ -1,12 +1,18 @@
 ﻿#include <iostream>
 #include <vector>
+#include <memory>
+#include <algorithm>
+#include <random>
+
+enum Mast : char { CHERVY = 3, BUBY = 4, KRESTY = 5, PIKY = 6 };
+enum CostCard : char { ACE = 1, TWO = 2, TREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10, JACK = 10, QUEEN = 10, KING = 10 };
+std::vector<Mast> arrMast = { CHERVY, BUBY,  KRESTY, PIKY };
+std::vector<CostCard> arrCard = { ACE, TWO, TREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
 
 class Card
 {
 private:
-	enum Mast : char { CHERVY = 3, BUBY = 4, KRESTY = 5, PIKY = 6 };
-	enum CostCard : int { ACE = 1, TWO = 2, TREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10, JACK = 10, QUEEN = 10, KING = 10 };
-	Mast mast = BUBY;
+	Mast mast = CHERVY;
 	CostCard cost = ACE;
 	bool crap = false;
 public:
@@ -46,10 +52,12 @@ class GenericPlayer : public Hand {
 private:
 	std::string name = "";
 public:
-	virtual bool const IsHitting();
+	virtual bool const IsHitting() = 0;
 	bool IsBoosted() { GetValue() > 21 ? true : false; };
 	void Bust() { std::cout << this->name << "Has a bust!" << std::endl; };
+	void setName(std::string& name) { this->name = name; };
 	std::string  getName() const { return name; };
+	
 };
 
 class Player : public GenericPlayer {
@@ -69,8 +77,7 @@ public:
 	void Lose() const { std::cout << getName() << " is Looser!"; };
 	void Push() const { std::cout << "Have no winer!"; };
 };
-//• virtual bool IsHitting() const - метод указывает, нужна ли дилеру еще одна карта.Если у дилера не больше 16 очков, то он берет еще одну карту.
-//• void FlipFirstCard() - метод переворачивает первую карту дилера.
+
 class House : public GenericPlayer {
 public:
 	bool const IsHitting() override {
@@ -80,6 +87,113 @@ public:
 		if (GetCrap()) { Flip(); };	
 	};
 };
+
+//3. Создать класс Deck, который наследует от класса Hand и представляет собой колоду карт.Класс Deck имеет 4 метода:
+//• vold Populate() - Создает стандартную колоду из 52 карт, вызывается из конструктора.
+//• void Shuffle() - Метод, который тасует карты, можно использовать функцию из алгоритмов STL random_shuffle
+//• vold Deal(Hand& aHand) - метод, который раздает в руку одну карту
+//• void AddltionalCards(GenericPlayer& aGenerlcPlayer) - раздает игроку дополнительные карты до тех пор, пока он может и хочет их получать
+//
+//Обратите внимание на применение полиморфизма.В каких методах применяется этот принцип ООП ?
+
+class Deck : public Hand {
+private:
+	int myLeight = 51;
+	int firstElement = 0;
+	int lastElement;
+	std::vector<Card*> myDeck;
+public:
+
+	Deck() { Populate(); };
+	void Populate() {
+		//myDeck = new Card[this->myLeight];
+		int nextElement = 0;
+		for (int i = 0; i < arrMast.size(); i++)
+		{
+			for (int j = 0; j < arrCard.size(); j++)
+			{
+				Card myCard;
+				myCard.SetMast(arrMast[i]);
+				myCard.SetCostCard(arrCard[j]);
+
+				myDeck.push_back(&myCard);
+				//myDeck[nextElement++] = myCard;
+			}
+			this->firstElement = nextElement;
+		}
+	};
+
+	void Shuffle(){
+		auto rnd = std::default_random_engine{};
+		std::shuffle(std::begin(myDeck), std::end(myDeck), rnd);
+	};
+
+	void Deal(Hand& aHand) {
+		aHand.addCard(*myDeck.end());
+		myDeck.pop_back();
+	};
+
+	void AddItionalCards(GenericPlayer& aGenericPlayer) {
+		if (aGenericPlayer.IsHitting()){
+			Deal(aGenericPlayer);
+		}
+	};
+		
+};
+ 
+
+//4. Реализовать класс Game, который представляет собой основной процесс игры.У этого класса будет 3 поля :
+//	• колода карт
+//	• рука дилера
+//	• вектор игроков.
+//	Конструктор класса принимает в качестве параметра вектор имен игроков и создает объекты самих игроков.
+// В конструкторе создается колода карт и затем перемешивается.
+//	Также класс имеет один метод play().В этом методе раздаются каждому игроку по две стартовые карты, а первая карта дилера прячется.
+//Далее выводится на экран информация о картах каждого игра, в т.ч.и для дилера.
+//Затем раздаются игрокам дополнительные карты.
+// Потом показывается первая карта дилера и дилер набирает карты, если ему надо.
+//После этого выводится сообщение, кто победил, а кто проиграл.В конце руки всех игроков очищаются.
+
+class Game {
+private:
+	std::vector<Player> player;
+	Deck deck;
+	House house;
+public:
+	Game() {};
+	Game(std::vector<std::string> plr) {
+		for (auto element : plr) {
+			Player p;
+			p.setName(element);
+			player.push_back(p);
+		}
+		deck.Shuffle();
+	};
+
+	void play() {
+		for (size_t i = 0; i < 2; i++)
+		{
+			for (auto element : player) {
+				deck.Deal(element);
+			}
+			deck.Deal(house);
+			if (i == 0) { house.Flip(); }
+		}
+		
+
+	
+	};
+
+
+};
+
+
+
+
+//	5. 5. Написать функцию main() к игре Блекджек.
+//В этой функции вводятся имена игроков.
+//Создается объект класса Game и запускается игровой процесс.
+//Предусмотреть возможность повторной игры.
 
 int main()
 {
---
+++ HM_WH_1/HM_WH_1.cpp
@@ -0,0 +1,199 @@
+﻿#include <iostream>
+#include <string>
+#include <tuple>
+#include <optional>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+
+
+//===========================
+// Структура для ФИО
+//===========================
+struct Person{
+    std::string lastName;
+    std::string firstName;
+    std::optional<std::string> middleName;
+} person;
+
+//===========================
+// Структура для телефона
+//===========================
+struct PhoneNumber{
+    uint16_t countryCode;
+    uint16_t cityCode;
+    std::string number;
+    std::optional<uint16_t> externNum;
+} phoneNumber;
+
+//============================
+// Операторы сравнения 
+// Телефон и ФИО
+//============================
+bool operator==(const Person& p1, const Person& p2) {
+    return  tie(p1.firstName, p1.lastName, p1.middleName) == tie(p2.firstName, p2.lastName, p2.middleName);
+}
+bool operator<(const Person& p1, const Person& p2) {
+    return  tie(p1.firstName, p1.lastName, p1.middleName) < tie(p2.firstName, p2.lastName, p2.middleName);
+}
+bool operator==(const PhoneNumber& p1, const PhoneNumber& p2) {
+    return tie(p1.countryCode, p1.cityCode, p1.number, p1.externNum) == tie(p2.countryCode, p2.cityCode, p2.number, p2.externNum);
+}
+
+bool operator<(const PhoneNumber& p1, const PhoneNumber& p2) {
+    return tie(p1.countryCode, p1.cityCode, p1.number, p1.externNum) < tie(p2.countryCode, p2.cityCode, p2.number, p2.externNum);
+}
+//----------------------------
+
+//===========================
+// Операторы вывода на печать
+// Телефон и ФИО
+//===========================
+std::ostream& operator<<(std::ostream& out, const PhoneNumber& pn) {
+    out << "\t\t+" << pn.countryCode << "(" << pn.cityCode << ")" << pn.number;
+    if (pn.externNum.has_value()) {
+        out << " " << pn.externNum.value();
+    }
+    return out;
+};
+std::ostream& operator<<(std::ostream& out, const Person& pn) {
+    out << pn.lastName << "\t\t" << pn.firstName;
+    if (pn.middleName.has_value()) {
+        out << "\t\t " << pn.middleName.value();
+    }
+    return out;
+};
+//---------------------------
+
+//===========================
+// Класс записной книжки
+//===========================
+class PhoneBook {
+private:
+    std::vector<std::pair<Person, PhoneNumber>> phoneBook;
+
+public:
+    // Конструктор класса принимает открытый поток файла
+    // и заполняет поля структуры
+    PhoneBook(std::ifstream& str) {
+        std::stringstream myStream;
+        std::string line = "";
+        std::string sTmp;   // вспомогательная переменная чтобы положить данные в переменную оптионал-стринг
+        int num;            // вспомогательная переменная чтобы положить данные в переменную оптионал-инт
+        
+        while (str.is_open() && !str.eof()) {
+            std::getline(str, line);
+            if (line.find("+") > 0) { // проверяем есть ли + внутри строки
+                myStream << line.substr(0, line.find("+")); // в поток положим все данные ДО +
+                myStream >> person.lastName >> person.firstName >> sTmp;
+                if (!sTmp.empty()) { person.middleName = sTmp; }
+                myStream.clear();
+
+                myStream << line.substr(line.find("+"), line.rfind("+")); // в поток положим все данные ПОСЛЕ +
+                myStream >> phoneNumber.countryCode >> phoneNumber.cityCode >> phoneNumber.number >> num;
+                if (num != 0) { phoneNumber.externNum = num; }
+                myStream.clear();
+
+                //Заносим пару в вектор Записная книга 
+                phoneBook.push_back(std::make_pair(person, phoneNumber));
+            }
+            line.erase();
+        }
+        str.close();
+    };
+
+    // Вывод данных на экран через перегруженные операторы вывода.
+    friend std::ostream& operator<<(std::ostream& out, PhoneBook& pb) {
+        for (auto it : pb.phoneBook) {
+            out << it.first << it.second << std::endl;
+        }
+        return out;
+    };
+
+    // Сортировка через перегруженные операторы сравнения    
+    void SortByName() {
+        std::sort(this->phoneBook.begin(), this->phoneBook.end(),
+            [](std::pair<Person, PhoneNumber> p1, std::pair<Person, PhoneNumber> p2) {return p1.first < p2.first;});
+    };
+    void SortByPhon() {
+        std::sort(this->phoneBook.begin(), this->phoneBook.end(),
+            [](std::pair<Person, PhoneNumber> p1, std::pair<Person, PhoneNumber> p2) {return p1.second < p2.second;});
+    };
+
+    // Поиск номера по фамилии
+    void GetPhoneNumber(std::string& lName) {
+        int count = 0; // счетчик сколько нашли совпадений
+
+        for (auto pr : this->phoneBook) {
+            if (lName == pr.first.lastName) { count++; };
+        }
+        switch (count)
+        {
+        case (0):
+            std::cout << lName << " not found" << std::endl;
+            break;
+        case (1):
+            for (auto pr : this->phoneBook) {
+                if (lName == pr.first.lastName) {
+                    std::cout << pr.first << pr.second << std::endl;
+                }
+            }
+            break;
+        default:
+            std::cout << lName << " found more than 1" << std::endl;
+            break;
+        }
+    };
+
+    // Замена номера телефона человека
+    void ChangePhoneNumber(Person& prChng, PhoneNumber& pnChng) {
+        int i = 0;
+        for (auto el : this->phoneBook)
+        {
+            if (el.first == prChng) { this->phoneBook[i] = std::make_pair(prChng, pnChng); }
+            i++;
+        }
+    }
+
+};
+//---------------------------
+
+int main()
+{
+    std::ifstream  dataToPars("Phonebook1.txt", std::ios_base::in);
+    PhoneBook book(dataToPars);
+    std::cout << "--------PhonBook----------" << std::endl;
+    std::cout << book << std::endl;
+    std::cout << "--------------------------" << std::endl;
+
+    std::cout << "--------SortByName--------" << std::endl;
+    book.SortByName();
+    std::cout << book << std::endl;
+    std::cout << "--------------------------" << std::endl;
+
+    std::cout << "--------SortByPhon--------" << std::endl;
+    book.SortByPhon();
+    std::cout << book << std::endl;
+    std::cout << "--------------------------" << std::endl;
+
+    std::cout << "--------GetNomber---------" << std::endl;
+    std::string nameToFind = "Dubinin";
+    book.GetPhoneNumber(nameToFind);
+    nameToFind = "Leo";
+    book.GetPhoneNumber(nameToFind);
+    std::cout << "--------------------------" << std::endl;
+
+    std::cout << "-------ChangeNomber-------" << std::endl;
+    Person perToChange;
+    PhoneNumber phnToChange;
+    //Dubinin Aleksei Mikhailovich + 7 473 7449054
+    nameToFind = "Dubinin";
+
+    perToChange = { "Dubinin", "Aleksei", "Mikhailovich"} ;
+    phnToChange = { 3, 999, "1112233", std::nullopt };
+        
+    book.ChangePhoneNumber(perToChange, phnToChange);
+    book.GetPhoneNumber(nameToFind);
+    std::cout << "--------------------------" << std::endl;
+}
